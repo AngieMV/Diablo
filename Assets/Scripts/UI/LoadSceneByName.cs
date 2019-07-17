ab8d29abbcb96e36112e60967fed7fed562c9b1e
angiemx <angie.saudade@gmail.com>
2019-07-16 17:52:18 -0700

    Adding a main menu

---
+++ Assets/Scripts/UI/LoadSceneByName.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneByName : MonoBehaviour
+{
+    [SerializeField]
+    private string _SceneName;
+
+    [SerializeField]
+    private bool _LoadOnEnable;
+
+    private void OnEnable()
+    {
+        if(_LoadOnEnable)
+        {
+            LoadScene();
+        }
+    }
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(_SceneName);
+    }
+}
