9403ff56943cdedfe0b61d9990471076632ca3f0
Angelica Mendez <angie.saudade@gmail.com>
2019-07-18 10:59:56 -0700

    updating camera parameters

--- Assets/Scripts/UI/CameraFollow.cs
+++ Assets/Scripts/UI/CameraFollow.cs
@@ -9,9 +9,9 @@ public class CameraFollow : MonoBehaviour
     private float _Smoothing = 10f;
 
     [SerializeField]
-    private Vector3 offset = new Vector3(0f, 12f, 0f);
+    private Vector3 _Offset = new Vector3(0f, 15f, 0f);
 
-    private Vector3 playerCameraControlOffset = new Vector3();
+    private Vector3 _PlayerCameraControlOffset = new Vector3();
 
     private void FixedUpdate()
     {
@@ -20,7 +20,7 @@ public class CameraFollow : MonoBehaviour
             return;
         }
 
-        Vector3 targetPosition = this._Transform.Value.position + this.offset + this.playerCameraControlOffset;
+        Vector3 targetPosition = this._Transform.Value.position + this._Offset + this._PlayerCameraControlOffset;
         this.transform.position = Vector3.Lerp(this.transform.position, targetPosition, this._Smoothing * Time.deltaTime);
     }
 }
