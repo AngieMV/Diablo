3ffdd0769ec90078243bce1fa217d473fa44ff3f
angiemx <angie.saudade@gmail.com>
2019-07-18 14:36:37 -0700

    Destroying health bar when enemie die

---
+++ Assets/Scripts/UI/DestroyBar.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyBar : MonoBehaviour
+{
+    [SerializeField]
+    private HealthValue health;
+
+    private void Update() 
+    {
+        if (health == null)
+        {
+            Destroy(gameObject);
+        }    
+    }
+}
